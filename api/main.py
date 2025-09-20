d4647418011fb86e90d1afa4e966d32784dc95ac
kimzhong <652269930@qq.com>
2025-09-20 17:15:11 +0800

    feat: add database connection event handlers

--- api/main.py
+++ api/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from api.api import tasks, users
 from core.config import settings
+from db.database import connect_to_mongo, close_mongo_connection
 
 app = FastAPI()
+
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
 
 app.include_router(tasks.router, prefix=settings.API_V1_STR, tags=["tasks"])
 app.include_router(users.router, prefix=settings.API_V1_STR, tags=["users"])
