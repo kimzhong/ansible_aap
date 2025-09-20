78c821bc6f1efeef21f3186ee2b70598656fa6be
kimzhong <652269930@qq.com>
2025-09-20 17:29:22 +0800

    feat: add login route and update App.vue to use router-view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,8 @@
 <template>
-  <el-container style="height: 100vh;">
-    <el-header style="background-color: #409EFF; color: white; display: flex; align-items: center; justify-content: center;">
-      <h1>Ansible AAP</h1>
-    </el-header>
-    <el-main>
-      <div v-if="isLoggedIn">
-        <AnsibleRunner />
-      </div>
-      <Login v-else @login-success="handleLoginSuccess" />
-    </el-main>
-    <el-footer style="background-color: #f2f2f2; text-align: center; padding: 10px;">
-      <p>© 2024 Ansible AAP</p>
-    </el-footer>
-  </el-container>
+  <router-view />
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import Login from './components/Login.vue';
-import AnsibleRunner from './components/AnsibleRunner.vue';
-
-const isLoggedIn = ref(true); // Hardcoded for now
-
-const handleLoginSuccess = () => {
-  isLoggedIn.value = true;
-};
 </script>
 
 <style>
