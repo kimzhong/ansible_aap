14b7ddb4176502a963a765a03fa48b3149261677
kimzhong <652269930@qq.com>
2025-09-20 17:28:21 +0800

    feat: add login page view

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input type="email" v-model="email" placeholder="Email" required>
+      <input type="password" v-model="password" placeholder="Password" required>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      // Handle login logic here
+      console.log('Logging in with', this.email, this.password)
+    }
+  }
+}
+</script>
